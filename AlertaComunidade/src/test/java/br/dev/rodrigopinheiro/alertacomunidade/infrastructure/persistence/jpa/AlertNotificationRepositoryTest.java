ba5104adaa2580d0e7815eb396987155ebd86e75
rpsouza441 <rpsouza@hotmail.co.uk>
2025-06-03 19:25:28 -0300

    Feito o JpaAlertRepositoryTest

--- AlertaComunidade/src/test/java/br/dev/rodrigopinheiro/alertacomunidade/infrastructure/persistence/jpa/AlertNotificationRepositoryTest.java
+++ AlertaComunidade/src/test/java/br/dev/rodrigopinheiro/alertacomunidade/infrastructure/persistence/jpa/AlertNotificationRepositoryTest.java
@@ -4,7 +4,6 @@ import br.dev.rodrigopinheiro.alertacomunidade.AlertaComunidadeApplication;
 import br.dev.rodrigopinheiro.alertacomunidade.domain.enums.AlertStatus;
 import br.dev.rodrigopinheiro.alertacomunidade.domain.enums.AlertType;
 import br.dev.rodrigopinheiro.alertacomunidade.domain.model.AlertNotification;
-import br.dev.rodrigopinheiro.alertacomunidade.infrastructure.persistence.jpa.SpringDataAlertRepository;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
