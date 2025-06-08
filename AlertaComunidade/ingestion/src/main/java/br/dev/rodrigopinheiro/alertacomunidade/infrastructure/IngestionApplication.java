4b6752e000e7d7d953566d85c7f4ec00d44e24a5
rpsouza441 <rpsouza@hotmail.co.uk>
2025-06-08 15:34:43 -0300

    Finalizado a migracao do ingestion

---
+++ AlertaComunidade/ingestion/src/main/java/br/dev/rodrigopinheiro/alertacomunidade/infrastructure/IngestionApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigopinheiro.alertacomunidade.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestionApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestionApplication .class, args);
+	}
+
+}
--- AlertaComunidade/ingestion/src/test/java/br/dev/rodrigopinheiro/alertacomunidade/infrastructure/rest/AlertControllerTest.java
+++ AlertaComunidade/ingestion/src/test/java/br/dev/rodrigopinheiro/alertacomunidade/infrastructure/rest/AlertControllerTest.java
@@ -27,7 +27,6 @@ import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
