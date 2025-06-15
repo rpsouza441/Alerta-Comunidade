041e9909cf2a3ec7ff924e38ae0145b2957e9d15
rpsouza441 <rpsouza@hotmail.co.uk>
2025-06-15 16:50:38 -0300

    Create SubscriberRequestDTO.java

---
+++ AlertaComunidade/src/main/java/br/dev/rodrigopinheiro/alertacomunidade/dto/SubscriberRequestDTO.java
@@ -0,0 +1,19 @@
+package br.dev.rodrigopinheiro.alertacomunidade.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+public class SubscriberRequestDTO {
+    @Email(message = "Email inválido")
+    private String email;
+
+    @Pattern(regexp = "^\\+?\\d{10,15}$", message = "Telefone inválido")
+    private String phoneNumber;
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+}
