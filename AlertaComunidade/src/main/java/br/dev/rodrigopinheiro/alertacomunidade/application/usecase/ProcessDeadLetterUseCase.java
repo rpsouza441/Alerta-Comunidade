0c245c3a28445a83e2f1c63313e2c8b48a46c9f3
MercyMonkey <45498863+MercyMonkey@users.noreply.github.com>
2025-06-18 17:27:32 -0300

    Create ProcessDeadLetterUseCase.java

---
+++ AlertaComunidade/src/main/java/br/dev/rodrigopinheiro/alertacomunidade/application/usecase/ProcessDeadLetterUseCase.java
@@ -0,0 +1,31 @@
+package br.dev.rodrigopinheiro.alertacomunidade.application.usecase;
+
+import br.dev.rodrigopinheiro.alertacomunidade.domain.model.DeadLetterMessage;
+import br.dev.rodrigopinheiro.alertacomunidade.domain.port.input.ProcessDeadLetterInputPort;
+import br.dev.rodrigopinheiro.alertacomunidade.domain.port.output.DeadLetterRepositoryPort;
+import br.dev.rodrigopinheiro.alertacomunidade.dto.DeadLetterRequestDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProcessDeadLetterUseCase implements ProcessDeadLetterInputPort {
+    private static final Logger logger = LoggerFactory.getLogger(ProcessDeadLetterUseCase.class);
+    private final DeadLetterRepositoryPort repository;
+
+    public ProcessDeadLetterUseCase(DeadLetterRepositoryPort repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public DeadLetterMessage execute(DeadLetterRequestDTO dto) {
+        DeadLetterMessage message = new DeadLetterMessage();
+        message.setQueueName(dto.queueName());
+        message.setPayload(dto.paylod());
+        message.setHeaders(dto.headers());
+        DeadLetterMessage saved = repository.save(message);
+        logger.debug("Quarantined message persisted. id={}", saved.getId());
+        return saved;
+    }
+
+}
