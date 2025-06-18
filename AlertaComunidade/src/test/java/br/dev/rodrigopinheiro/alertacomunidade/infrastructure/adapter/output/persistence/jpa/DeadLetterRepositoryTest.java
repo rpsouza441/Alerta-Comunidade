e803f9e78932016b129b3f078b0a416d7f2e6ad8
MercyMonkey <45498863+MercyMonkey@users.noreply.github.com>
2025-06-18 17:28:21 -0300

    Create DeadLetterRepositoryTest.java

---
+++ AlertaComunidade/src/test/java/br/dev/rodrigopinheiro/alertacomunidade/infrastructure/adapter/output/persistence/jpa/DeadLetterRepositoryTest.java
@@ -0,0 +1,34 @@
+package br.dev.rodrigopinheiro.alertacomunidade.infrastructure.adapter.output.persistence.jpa;
+
+
+import br.dev.rodrigopinheiro.alertacomunidade.domain.model.DeadLetterMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+public class DeadLetterRepositoryTest {
+
+    @Autowired
+    private SpringDataDeadLetterRepository repository;
+
+    @Test
+    @DisplayName("Deve persistir DeadLetterMessage com sucesso")
+    void shouldPersist() {
+        DeadLetterMessage message = new DeadLetterMessage();
+        message.setQueueName("dlq.test");
+        message.setPayload("{}");
+        message.setHeaders("{}");
+
+        DeadLetterMessage saved = repository.save(message);
+        assertThat(saved.getId()).isNotNull();
+        assertThat(saved.getReceivedAt()).isNotNull();
+    }
+}
